b84130bb7b22ddc8e2ceb5a4eb28cc2072905b4c
mrlonelyjtr <296793179@qq.com>
2018-04-01 21:16:37 +0800

    iterator

---
+++ Iterator/BookShelfIterator.java
@@ -0,0 +1,19 @@
+public class BookShelfIterator implements Iterator {
+    private BookShelf bookShelf;
+    private int index;
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < bookShelf.getLength() ? true : false;
+    }
+
+    @Override
+    public Object next() {
+        return bookShelf.getBookAt(index++);
+    }
+}
