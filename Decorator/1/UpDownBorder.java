70d025ef07d85c639a082051a0c3079f5a8c1668
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-08-09 15:45:30 +0800

    decorator

---
+++ Decorator/1/UpDownBorder.java
@@ -0,0 +1,35 @@
+public class UpDownBorder extends Border {
+    private char borderChar;
+
+    public UpDownBorder(Display display, char ch) {
+        super(display);
+        this.borderChar = ch;
+    }
+
+    @Override
+    public int getColumns() {
+        return display.getColumns();
+    }
+
+    @Override
+    public int getRows() {
+        return display.getRows() + 2;
+    }
+
+    @Override
+    public String getRowText(int row) {
+        if (row == 0 || row == getRows() - 1)
+            return makeLine(borderChar, getColumns());
+        else
+            return display.getRowText(row - 1);
+    }
+
+    private String makeLine(char ch, int count) {
+        StringBuffer buf = new StringBuffer();
+
+        for (int i = 0; i < count; i++)
+            buf.append(ch);
+
+        return buf.toString();
+    }
+}
