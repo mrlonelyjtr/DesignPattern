1dcffbd3244304413e0f9ac8dd5cb1a89c3fdf2b
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-08-22 22:53:51 +0800

    facade

---
+++ Facade/2/Main.java
@@ -0,0 +1,8 @@
+import pagemaker.PageMaker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PageMaker.makeLinkPage("linkpage.html");
+    }
+}
---
+++ Facade/2/pagemaker/PageMaker.java
@@ -0,0 +1,28 @@
+package pagemaker;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PageMaker {
+    private PageMaker() {}
+
+    public static void makeLinkPage(String filename) {
+        try {
+            HtmlWriter writer = new HtmlWriter(new FileWriter(filename));
+            writer.title("Link page");
+            Properties mailprop = Database.getProperties("./src/maildata");
+            Enumeration en = mailprop.propertyNames();
+            while (en.hasMoreElements()) {
+                String mailaddr = (String) en.nextElement();
+                String username = mailprop.getProperty(mailaddr);
+                writer.mailto(mailaddr, username);
+            }
+            writer.close();
+            System.out.println(filename + " is created.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
