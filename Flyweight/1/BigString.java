26089dfbce8345ce0e0470651d838121ead59895
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-08-28 22:58:14 +0800

    flyweight

---
+++ Flyweight/1/BigString.java
@@ -0,0 +1,34 @@
+public class BigString {
+    private BigChar[] bigChars;
+
+    public BigString(String string) {
+        initShared(string);
+    }
+
+    public BigString(String string, boolean shared) {
+        if (shared)
+            initShared(string);
+        else
+            initUnshared(string);
+    }
+
+    private void initShared(String string) {
+        bigChars = new BigChar[string.length()];
+        BigCharFactory factory = BigCharFactory.getInstance();
+
+        for (int i = 0; i < string.length(); i++)
+            bigChars[i] = factory.getBigChar(string.charAt(i));
+    }
+
+    private void initUnshared(String string) {
+        bigChars = new BigChar[string.length()];
+
+        for (int i = 0; i < string.length(); i++)
+            bigChars[i] = new BigChar(string.charAt(i));
+    }
+
+    public void print() {
+        for (int i = 0; i < bigChars.length; i++)
+            bigChars[i].print();
+    }
+}
