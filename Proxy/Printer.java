5997dfb1a83aec4a828df795b456c28cab8151b8
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-08-29 17:29:19 +0800

    proxy

---
+++ Proxy/Printer.java
@@ -0,0 +1,43 @@
+public class Printer implements Printable {
+    private String name;
+
+    public Printer() {
+        heavyJob("Printer的实例生成中");
+    }
+
+    public Printer(String name) {
+        this.name = name;
+        heavyJob("Printer的实例生成中（"+ name + "）");
+    }
+
+    @Override
+    public void setPrinterName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getPrinterName() {
+        return name;
+    }
+
+    @Override
+    public void print(String string) {
+        System.out.println("=== " + name + " ===");
+        System.out.println(string);
+    }
+
+    private void heavyJob(String msg) {
+        System.out.print(msg);
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+            System.out.print(".");
+        }
+
+        System.out.println("结束。");
+    }
+}
---
+++ Proxy/PrinterProxy.java
@@ -0,0 +1,42 @@
+public class PrinterProxy implements Printable {
+    private String name;
+    private String className;
+    private Printable real;
+
+    public PrinterProxy(String name, String className) {
+        this.name = name;
+        this.className = className;
+    }
+
+    @Override
+    public synchronized void setPrinterName(String name) {
+        if (real != null)
+            real.setPrinterName(name);
+
+        this.name = name;
+    }
+
+    @Override
+    public String getPrinterName() {
+        return name;
+    }
+
+    @Override
+    public void print(String string) {
+        realize();
+        real.print(string);
+    }
+
+    private synchronized void realize() {
+        if (real == null) {
+            try {
+                real = (Printable) Class.forName(className).newInstance();
+                real.setPrinterName(name);
+            } catch (ClassNotFoundException e) {
+                System.out.println("没有找到" + className + "类。");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
